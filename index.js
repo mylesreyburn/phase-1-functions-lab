9ded604faca91a37e59e44de3ad4aed85dcba31d
Myles Reyburn <mylesreyburn@gmail.com>
2023-01-31 19:34:05 -0700

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solution in this file!
+
+// one block = 264 feet
+// scuber HQ is on 42nd street
+
+function distanceFromHqInBlocks(street){
+    let distance = (42-street);
+    if (distance < 0){
+        distance = distance * (-1)
+    }
+    return distance;
+}
+
+function distanceFromHqInFeet(street){
+    let distance = (42-street)*264;
+    if (distance < 0){
+        distance = distance * (-1)
+    }
+    return distance;
+}
+
+function distanceTravelledInFeet(start, end){
+    let distance = (start-end)*264;
+    if (distance < 0){
+        distance = distance * (-1)
+    }
+    return distance;
+}
+
+function calculatesFarePrice(start, destination){
+    let distance = (start-destination)*264;
+    let fare = 0;
+    if (distance < 0){
+        distance = distance * (-1)
+    }
+    if (distance <= 400){
+        fare = 0;
+        return fare;
+    } else if (distance > 400 && distance <=2000){
+        fare = 0.02 * (distance - 400);
+        return fare;
+    } else if (distance > 2000 && distance <= 2500){
+        fare = 25;
+        return fare;
+    } else {
+        return "cannot travel that far"
+    }
+}
